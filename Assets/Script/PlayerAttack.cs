753023b235dc8122dd0cc238cbcb1a785c8314fe
DESKTOP-KP6T26F\1409A <samuel55555s123@gmail.com>
2026-04-28 16:29:48 +0800

    Update MainScene and PlayerAttack

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class PlayerAttack : MonoBehaviour
 {
     private Animator animator;
     private float attackCooldown = 0.6f;
     private float cooldownTimer = 0f;
+
+    [Header("Attack")]
+    public float attackRange = 1.5f;
+    public float attackRadius = 0.75f;
+    public int attackDamage = 1;
+    public LayerMask enemyLayers = ~0;
 
     void Start()
     {
@@ -20,7 +27,26 @@ public class PlayerAttack : MonoBehaviour
         if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame && cooldownTimer <= 0f)
         {
             animator.Play("Attack01");
+            DealDamage();
             cooldownTimer = attackCooldown;
+        }
+    }
+
+    void DealDamage()
+    {
+        Vector3 attackCenter = transform.position + transform.forward * attackRange;
+        Collider[] hits = Physics.OverlapSphere(attackCenter, attackRadius, enemyLayers, QueryTriggerInteraction.Collide);
+
+        HashSet<Damageable> damagedTargets = new HashSet<Damageable>();
+
+        foreach (Collider hit in hits)
+        {
+            Damageable damageable = hit.GetComponentInParent<Damageable>();
+            if (damageable == null || damagedTargets.Contains(damageable))
+                continue;
+
+            damageable.TakeDamage(attackDamage);
+            damagedTargets.Add(damageable);
         }
     }
 }
